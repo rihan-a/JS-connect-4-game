39490787ee8cac0e02d1e3e25d5134d4803c2800
rihan_a <a.rihan@live.com>
2022-10-17 19:57:54 +0200

    file cleaning

--- script.js
+++ script.js
@@ -221,33 +221,3 @@ function ShowWinningFour() {
             .addClass("winning-four");
     });
 }
-
-function points(games) {
-    // your code here
-    let score = 0;
-
-    for (let i = 0; i < games.length; i++) {
-        if (games[i][0] > games[i][2]) {
-            score += 3;
-        } else if (games[i][0] == games[i][2]) {
-            score++;
-        }
-    }
-
-    return score;
-}
-
-console.log(
-    points([
-        "1:0",
-        "2:0",
-        "3:0",
-        "4:0",
-        "2:1",
-        "3:1",
-        "4:1",
-        "3:2",
-        "4:2",
-        "4:3",
-    ])
-);
